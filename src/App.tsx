a1506c1c7cd5719946ecbaf0c929448f68d2fdb4
Gibson Murray <gibmurrays@gmail.com>
2024-09-19 12:19:25 -0400

    adasd

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ function App() {
         <iframe
             width="600"
             height="400"
-            src="https://vercel.com/gibsons-projects-133fb0b2/olympic-medals/FQ6Jt2d7Lo4dRUhB3y2AxB2j7Gz7"
+            src="https://olympic-medals.vercel.app"
         ></iframe>
     )
 }
